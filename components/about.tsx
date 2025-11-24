59863ebfd982427ca0da5e08c6d46b0d974c6aa0
Julia-proj <juleea0@gmail.com>
2025-11-24 15:51:43 +0100

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -38,7 +38,7 @@ export function About() {
               <p>
                 A lo largo de mi trayectoria he desarrollado dos cursos: el{" "}
                 <span className="font-medium text-neutral-900">curso de iniciación en manicura</span> y el{" "}
-                <span className="font-medium text-neutral-900">curso de Subida de cualificación</span>, ambos creados a
+                <span className="font-medium text-neutral-900">curso de subida de cualificación</span>, ambos creados a
                 partir de métodos prácticos y fáciles de aprender.
               </p>
             </div>
